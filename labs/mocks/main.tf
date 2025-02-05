20674df5eb4547f551563ce009f4d3e0c6ee451b
Your Name <you@example.com>
2025-02-05 14:07:23 +0000

    Updating mocks tries

--- labs/mocks/main.tf
+++ labs/mocks/main.tf
@@ -19,3 +19,16 @@ output "vm_yanin_info" {
   value      = data.aws_instance.yanim-vm.public_ip
   description = "yaniv-vm public ip"
 }
+
+
+data "aws_ami" "ami_version" {
+  filter {
+    name   = "name"
+    values = ["terraform-workshop-image-do-not-delete"]
+  }
+}
+
+output "ami_version_info_id" {
+  value = data.aws_ami.ami_version.image_id
+  description = "The version of ubuntu with ssh user + password"
+}

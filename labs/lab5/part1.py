11193e87438f1934948d2e56d8495dd20c86f5ef
Almog Madar(AMDev) <madar.almog@gmail.com>
2025-02-12 14:13:15 +0200

    subprocess part1 & part2

---
+++ labs/lab5/part1.py
@@ -0,0 +1,17 @@
+import subprocess
+
+command = "ls -la"
+
+try:
+    p = subprocess.run(command.split(),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+    if p.returncode == 0 :
+        print (p.stdout.decode())
+    else:
+        print(f"Error:\n{p.stderr.decode()}")
+
+except FileNotFoundError:
+    print("Error: Command not found!")
+except PermissionError:
+    print("Error: Permission denied!")
+

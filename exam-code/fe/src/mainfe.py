2e6db52cc5ab8072cc97b8936b8d82974ec57d31
Almog Madar(AMDev) <madar.almog@gmail.com>
2025-03-04 10:27:45 +0200

    fix lint

--- exam-code/fe/src/mainfe.py
+++ exam-code/fe/src/mainfe.py
@@ -14,14 +14,12 @@ BACKEND_API_URL = "http://backend-service:5001/fetch_price"
 def status():
     return jsonify({"status": "OK"}), 200
 
-
 @app.route('/')
 def index():
     """
     Render the index page.
     """
     return render_template("index.html")
-
 
 @app.route('/fetch_price', methods=['GET'])
 def fetch_price():
@@ -32,7 +30,6 @@ def fetch_price():
     if response.status_code == 200:
         return jsonify(response.json())
     return jsonify({"error": "Failed to fetch prices from backend"}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002, debug=False)
--- exam-code/fe/src/tests/test_main.py
+++ exam-code/fe/src/tests/test_main.py
@@ -1,11 +1,10 @@
 import sys
 import os
+import pytest
+from mainfe import app  # Assuming the frontend is a Flask app
 
 # Add the src directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-import pytest
-from mainfe import app  # Assuming the frontend is a Flask app
 
 @pytest.fixture
 def client():

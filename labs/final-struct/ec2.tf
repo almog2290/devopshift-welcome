3d608067c4c1ab235b2c2b177d30442c42c22bfd
almog2290(AMDev) <madar.almog@gmail.com>
2025-03-15 13:40:41 +0200

    Adding name tag to sg

--- labs/final-struct/ec2.tf
+++ labs/final-struct/ec2.tf
@@ -17,6 +17,10 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "${var.prefix_name}-ec2-sg"
+  }
 }
 
 resource "aws_instance" "builder_vm" {
